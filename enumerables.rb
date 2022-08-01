7082fdf2bfb1ccf2979770f84c53edbdf1a4cc7c
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-01 15:16:01 -0400

    added my_each

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,21 @@
 # created file
 # hello
 # my example comment
+
+class Array
+
+    def my_each(&prc)
+        
+        i = 0
+
+        while i < self.length
+            print prc.call(self[i])
+            i += 1
+        end
+
+        return self
+    end
+
+end
+
+# p return_value = [1, 2, 3].my_each { |num| puts num }.my_each { |num| puts num }
