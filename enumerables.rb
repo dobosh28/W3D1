41fc2870c6821cfc53407aee2b2a657c27f58116
Ryan Kok <3nayrkok5@gmail.com>
2022-08-01 16:19:15 -0400

    added my_select

--- enumerables.rb
+++ enumerables.rb
@@ -8,14 +8,29 @@ class Array
         
         i = 0
 
-        while i < self.length
-            print prc.call(self[i])
+        while i <= self.length-1
+           prc.call(self[i])
             i += 1
         end
 
-        return self
+       return self
+    end
+
+    def my_select(&prc)
+        new_arr = []
+        self.my_each do |ele| 
+            if prc.call(ele)
+                new_arr << ele
+            end
+        end
+        return new_arr 
     end
 
 end
 
 # p return_value = [1, 2, 3].my_each { |num| puts num }.my_each { |num| puts num }
+# p return_value = [1, 2, 3].my_each { |num| puts num }
+a = [1, 2, 3]
+
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
